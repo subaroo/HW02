f2a617ac92dde479ad6f25f72774a659dea2a758
danielsgriffin <danielsgriffin@gmail.com>
2015-08-05 07:32:29 -0700

    hw prepped

---
+++ HW02_ex03_05.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# HW02_ex03_05
+
+# This exercise can be done using only the statements and other features we 
+# have learned so far.
+
+# (1) Write a function that draws a grid like the following:
+# + - - - - + - - - - +
+# |         |         |
+# |         |         |
+# |         |         |
+# |         |         |
+# + - - - - + - - - - +
+# |         |         |
+# |         |         |
+# |         |         |
+# |         |         |
+# + - - - - + - - - - +
+# Hint: to print more than one value on a line, you can print a comma-separated
+# sequence:
+
+# print '+', '-'
+# If the sequence ends with a comma, Python leaves the line unfinished, so the 
+# value printed next appears on the same line.
+
+# print '+', 
+# print '-'
+
+# The output of these statements is '+ -'.
+
+# A print statement all by itself ends the current line and goes to the next line.
+
+# (2) Write a function that draws a similar grid with four rows and four columns.
+################################################################################
+# Write your functions below:
+# Body
+
+
+
+
+
+
+
+
+
+
+
+
+# Write your functions above:
+################################################################################
+def main():
+    """Call your functions within this function.
+    When complete have two function calls in this function:
+    two_by_two()
+    four_by_four()
+    """
+    print("Hello World!")
+    
+
+
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # HW02
+
+##Directions:
+
+1. fork
+1. clone
+1. **modify**
+1. add
+1. commit
+1. push
+1. pull-request
+
+##Exercises:
+###Think Python:
+####[Chapter 3: Functions][ch3]
+
+(do all exercises, submit the below)
++ HW02_ex03_03.py
++ HW02_ex03_04.py
++ HW02_ex03_05.py
+
+####[Chapter 5: Conditionals and Recursion][ch5]
++ HW02_ex05_02.py
++ HW02_ex05_03.py
++ HW02_ex05_04.py
+
+
+<!-- Links -->
+
+[ch3]: http://www.greenteapress.com/thinkpython/html/thinkpython004.html
+[ch5]: http://www.greenteapress.com/thinkpython/html/thinkpython006.html
